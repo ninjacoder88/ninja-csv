3d18e3acdb3f2485b507ef42e2dc33adcba1a6e0
Tommy Harris <tomharris@idtdna.com>
2020-04-02 22:08:03 -0500

    Added simple types

--- src/NinjaCsv.UnitTests/Utility/UnitTestItem.cs
+++ src/NinjaCsv.UnitTests/Utility/UnitTestItem.cs
@@ -2,6 +2,12 @@
 {
     public class UnitTestItem
     {
-        
+        public int IntId { get; set; }
+
+        public double DoubleId { get; set; }
+
+        public decimal DecimalId { get; set; }
+
+        public string StringId { get; set; }
     }
 }
