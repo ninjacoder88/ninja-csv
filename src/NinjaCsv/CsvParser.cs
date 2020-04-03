1272100b0f5f99802ec287f355cb1d4f8b997bb7
Tommy Harris <tomharris@idtdna.com>
2020-04-02 20:21:10 -0500

    Removed default class; Added CsvParser and ctor

---
+++ src/NinjaCsv/CsvParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace NinjaCsv
+{
+    public class CsvParser
+    {
+        private string _filePath;
+
+        public CsvParser(string filePath)
+        {
+            if (filePath == null)
+                throw new ArgumentNullException(nameof(filePath));
+
+            if(filePath == string.Empty)
+                throw new ArgumentException($"{nameof(filePath)} cannot be empty");
+
+            if(!File.Exists(filePath))
+                throw new ArgumentException($"The file path {filePath} does not exist");
+
+            _filePath = filePath;
+        }
+    }
+}
