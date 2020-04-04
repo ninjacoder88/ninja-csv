726a7ba8db7f78be07aa71c82a05751ebd0ec5b0
Tommy Harris <tomharris@idtdna.com>
2020-04-04 13:23:36 -0500

    Extracted logic into classes

--- src/NinjaCsv/CsvParser.cs
+++ src/NinjaCsv/CsvParser.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
+using NinjaCsv.Internal.Interfaces;
 
 namespace NinjaCsv
 {
     public class CsvParser
     {
-        public IEnumerable<T> Parse<T>(string filePath, CsvParserOptions options = default)
+        public CsvParser()
+        {
+        }
+
+        internal IFileLineProcessor FileLineProcessor
+        {
+            get => _fileLineProcessor ?? (_fileLineProcessor = new FileLineProcessor());
+            set => _fileLineProcessor = value;
+        }
+
+        internal INameToCsvMapper NameToCsvMapper
+        {
+            get => _nameToCsvMapper ?? (_nameToCsvMapper = new NameToCsvMapper());
+            set => _nameToCsvMapper = value;
+        }
+
+        internal ISystemFile SystemFile
+        {
+            get => _systemFile ?? (_systemFile = new SystemFile());
+            set => _systemFile = value;
+        }
+
+        public IEnumerable<T> Parse<T>(string filePath, CsvParserOptions options = null)
         {
             if (filePath == null)
                 throw new ArgumentNullException(nameof(filePath));
@@ -16,116 +37,61 @@ namespace NinjaCsv
             if (filePath == string.Empty)
                 throw new ArgumentException($"{nameof(filePath)} cannot be empty");
 
-            if (!File.Exists(filePath))
+            if (!_systemFile.Exists(filePath))
                 throw new ArgumentException($"The file path {filePath} does not exist");
+
+            var fileLines = _systemFile.ReadAllLines(filePath);
+            if (fileLines == null)
+            {
+                throw new InvalidOperationException($"Reading lines from {filePath} returned no results");
+            }
+
+            var fileLineList = fileLines.ToList();
+
+            if (!fileLineList.Any())
+            {
+                return new List<T>();
+            }
 
             if (options == null)
             {
                 options = new CsvParserOptions();
             }
 
-            var fileLines = File.ReadAllLines(filePath).ToList();
-
             if (options.ContainsHeaderRow)
-                fileLines = fileLines.Skip(1).ToList();
-
-            var nameForPosition = new Dictionary<int, string>();
+                fileLineList = fileLineList.Skip(1).ToList();
 
             var properties = typeof(T).GetProperties();
 
-            foreach (var propertyInfo in properties)
+            var nameForPosition = NameToCsvMapper.Map(properties);
+            if (nameForPosition == null)
             {
-                //TODO: make sure its simple value type not reference types or structs, https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/types#simple-types
-
-                var attributes = propertyInfo.GetCustomAttributes(false);
-                var attribute = attributes.FirstOrDefault();
-                if (attribute == null)
-                {
-                    continue;
-                }
-
-                var column = attribute as Column;
-                //TODO: check for null?
-                nameForPosition.Add(column.Position, propertyInfo.Name);
+                throw new InvalidOperationException("Failed to map");//TODO: better exception
             }
 
-            var items = new List<T>();
-            for (var l = 0; l < fileLines.Count; l++)
+            var nameForPositionDictionary = nameForPosition.ToDictionary(k => k.Key, v => v.Value);
+
+            if (!nameForPositionDictionary.Any())
             {
-                var fileLine = fileLines[l];
-                var splitFileLine = fileLine.Split(new[] {options.Delimiter}, StringSplitOptions.None);
+                //exception?
+            }
+            
+            var items = new List<T>();
+            for (var l = 0; l < fileLineList.Count; l++)
+            {
+                var fileLine = fileLineList[l];
 
-                var instance = Activator.CreateInstance(typeof(T));
-                var instanceType = instance.GetType();
+                var instance = FileLineProcessor.Process<T>(fileLine, options.Delimiter, nameForPositionDictionary);
+                //check for null?
 
-                for (var i = 0; i < splitFileLine.Length; i++)
-                {
-                    var c = splitFileLine[i];
-
-                    if (!nameForPosition.ContainsKey(i))
-                    {
-                        continue;
-                    }
-
-                    var value = nameForPosition[i];
-                    var instancePropertyInfo = instanceType.GetProperty(value, BindingFlags.Public | BindingFlags.Instance);
-                    if (instancePropertyInfo == null)
-                    {
-                        continue;
-                    }
-
-                    var declaringType = instancePropertyInfo.PropertyType;
-                    object finalValue;
-                    
-                    //TODO: this can be improved, no but really
-                    if (declaringType == typeof(int))
-                    {
-                        if (int.TryParse(c, out int intValue))
-                        {
-                            finalValue = intValue;
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                    else if (declaringType == typeof(double))
-                    {
-                        if (double.TryParse(c, out double doubleValue))
-                        {
-                            finalValue = doubleValue;
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                    else if (declaringType == typeof(decimal))
-                    {
-                        if (decimal.TryParse(c, out decimal decimalValue))
-                        {
-                            finalValue = decimalValue;
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                    //TODO: bool
-                    //TODO: long
-                    //TODO: see https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/value-types
-                    else
-                    {
-                        finalValue = c;
-                    }
-
-                    instancePropertyInfo.SetValue(instance, finalValue);
-                }
-
-                items.Add((T)instance);
+                items.Add((T) instance);
             }
 
             return items;
         }
+
+        private IFileLineProcessor _fileLineProcessor;
+        private INameToCsvMapper _nameToCsvMapper;
+        private ISystemFile _systemFile;
     }
 }
