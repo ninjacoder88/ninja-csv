2865e41991e0acbde20946614149b235b0e44446
Tommy Harris <tomharris@idtdna.com>
2020-04-04 13:22:40 -0500

    Extracted integration test content

---
+++ src/NinjaCsv.IntegrationTests/Utility/UnitTestItem.cs
@@ -0,0 +1,13 @@
+﻿namespace NinjaCsv.UnitTests.Utility
+{
+    public class UnitTestItem
+    {
+        public int IntId { get; set; }
+
+        public double DoubleId { get; set; }
+
+        public decimal DecimalId { get; set; }
+
+        public string StringId { get; set; }
+    }
+}
