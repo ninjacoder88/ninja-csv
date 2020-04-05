24c01750d225bb1703d9297552dce437ba3e0a1c
Tommy Harris <tomharris@idtdna.com>
2020-04-04 20:47:27 -0500

    Renamed

---
+++ src/NinjaCsv/Internal/PropertyNameToColumnMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NinjaCsv.Internal.Interfaces;
+
+namespace NinjaCsv
+{
+    internal class PropertyNameToColumnMapper : IPropertyNameToColumnMapper
+    {
+        public IEnumerable<KeyValuePair<int, string>> Map(PropertyInfo[] properties)
+        {
+            _ = properties ?? throw new ArgumentNullException(nameof(properties));
+
+            //foreach property
+            foreach (var propertyInfo in properties)
+            {
+                //get custom attributes on the property
+                var columnAttributes = propertyInfo.GetCustomAttributes(false);
+
+                int counter = 0;
+                foreach (var columnAttribute in columnAttributes)
+                {
+                    if(counter > 0)
+                        throw new InvalidOperationException($"{propertyInfo.Name} has multiple {nameof(Column)} attributes");
+                    if (!(columnAttribute is Column column))
+                        continue;
+                    counter++;
+
+                    //consider checking if propertyInfo.Name is null
+                    yield return new KeyValuePair<int, string>(column.Position, propertyInfo.Name);
+                }
+            }
+        }
+    }
+}
