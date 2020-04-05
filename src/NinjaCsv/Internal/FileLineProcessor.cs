60d7f05fafbb7feb93eb0c0a6e98fe33b9c72fd6
Tommy Harris <tomharris@idtdna.com>
2020-04-04 20:48:37 -0500

    Extracted cell parsing logic, additional validation

--- src/NinjaCsv/Internal/FileLineProcessor.cs
+++ src/NinjaCsv/Internal/FileLineProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using NinjaCsv.Internal.Interfaces;
 
@@ -7,79 +8,72 @@ namespace NinjaCsv
 {
     internal class FileLineProcessor : IFileLineProcessor
     {
-        public object Process<T>(string fileLine, string delimiter, Dictionary<int, string> nameForPosition)
+        public FileLineProcessor(ICellDataParser cellDataParser)
         {
+            _cellDataParser = cellDataParser ?? throw new ArgumentNullException(nameof(cellDataParser));
+        }
+
+        public object Process<T>(string fileLine, string delimiter, IReadOnlyDictionary<int, string> nameForPosition)
+        {
+            if (string.IsNullOrEmpty(fileLine))
+                throw new ArgumentException($"{nameof(fileLine)} must have a non-empty value");
+
+            if (string.IsNullOrEmpty(delimiter))
+                throw new ArgumentException($"{nameof(delimiter)} must have a non-empty value");
+
+            if (nameForPosition == null)
+                throw new ArgumentNullException(nameof(nameForPosition));
+
+            if (!nameForPosition.Any())
+                throw new ArgumentException($"{nameof(nameForPosition)} cannot be empty");
+
+            //create a new instance of the type
             var instance = Activator.CreateInstance(typeof(T));
-            var instanceType = instance.GetType();
 
-            var splitFileLine = fileLine.Split(new[] { delimiter }, StringSplitOptions.None);
+            //split the fileLine by the delimiter
+            var splitFileLine = fileLine.Split(new[] {delimiter}, StringSplitOptions.None);
 
+            //foreach fileLine
             for (var i = 0; i < splitFileLine.Length; i++)
             {
-                var c = splitFileLine[i];
+                //get the cell (value split by delimiter)
+                var cell = splitFileLine[i];
 
-                if (!nameForPosition.ContainsKey(i))
-                {
-                    continue;
-                }
-
-                var value = nameForPosition[i];
-
-                var instancePropertyInfo = instanceType.GetProperty(value, BindingFlags.Public | BindingFlags.Instance);
-                if (instancePropertyInfo == null)
-                {
-                    continue;
-                }
-
-                var declaringType = instancePropertyInfo.PropertyType;
-                object finalValue;
-
-                //TODO: this can be improved, no but really
-                if (declaringType == typeof(int))
-                {
-                    if (int.TryParse(c, out int intValue))
-                    {
-                        finalValue = intValue;
-                    }
-                    else
-                    {
-                        throw new Exception();
-                    }
-                }
-                else if (declaringType == typeof(double))
-                {
-                    if (double.TryParse(c, out double doubleValue))
-                    {
-                        finalValue = doubleValue;
-                    }
-                    else
-                    {
-                        throw new Exception();
-                    }
-                }
-                else if (declaringType == typeof(decimal))
-                {
-                    if (decimal.TryParse(c, out decimal decimalValue))
-                    {
-                        finalValue = decimalValue;
-                    }
-                    else
-                    {
-                        throw new Exception();
-                    }
-                }
-                //TODO: bool
-                //TODO: long
-                //TODO: see https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/value-types
-                else
-                {
-                    finalValue = c;
-                }
-
-                instancePropertyInfo.SetValue(instance, finalValue);
+                ParseCell(i, cell, instance, nameForPosition);
             }
 
             return instance;
         }
+
+        private void ParseCell(int i, string cell, object instance, IReadOnlyDictionary<int, string> nameForPosition)
+        {
+            var instanceType = instance.GetType();
+
+            if (!nameForPosition.TryGetValue(i, out string propertyName))
+            {
+                return;
+            }
+
+            var instancePropertyInfo = instanceType.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+            if (instancePropertyInfo == null)
+            {
+                //TODO: consider an exception here
+                return;
+            }
+
+            var instancePropertyType = instancePropertyInfo.PropertyType;
+
+            var finalValue = _cellDataParser.Parse(instancePropertyType, cell);
+
+            var finalValueType = finalValue.GetType().FullName;
+            if (instancePropertyType.FullName != finalValueType)
+            {
+                throw new InvalidOperationException($"The type of {propertyName} ({instancePropertyType.FullName}) does not match the parsed value of {finalValue} ({finalValueType})");
+            }
+
+            instancePropertyInfo.SetValue(instance, finalValue);
+        }
+
+        private readonly ICellDataParser _cellDataParser;
     }
 }
