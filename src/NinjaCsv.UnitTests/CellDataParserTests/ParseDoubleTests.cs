01db3574d0a0fef3a602c595acf60de2fc591c48
Tommy Harris <tomharris@idtdna.com>
2020-04-04 20:50:15 -0500

    Added tests

---
+++ src/NinjaCsv.UnitTests/CellDataParserTests/ParseDoubleTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using AutoFixture;
+using NUnit.Framework;
+
+namespace NinjaCsv.UnitTests.CellDataParserTests
+{
+    [TestFixture]
+    public class ParseDoubleTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void CellIsNull_ReturnsDefaultValue()
+        {
+            //SETUP
+            var sut = new CellDataParser();
+
+            //TEST
+            var result = sut.Parse(typeof(double), null);
+
+            //VALIDATE
+            Assert.That(result, Is.EqualTo(default(double)));
+        }
+
+        [Test]
+        public void CellIsEmpty_ReturnsDefaultValue()
+        {
+            //SETUP
+            var sut = new CellDataParser();
+
+            //TEST
+            var result = sut.Parse(typeof(double), string.Empty);
+
+            //VALIDATE
+            Assert.That(result, Is.EqualTo(default(double)));
+        }
+
+        [Test]
+        public void CellHasValidData_ReturnsValueAsInt()
+        {
+            //SETUP
+            var cellValue = _fixture.Create<double>();
+            var cellValueAsString = cellValue.ToString();
+            var sut = new CellDataParser();
+
+            //TEST
+            var result = sut.Parse(typeof(double), cellValueAsString);
+
+            //VALIDATE
+            Assert.That(result, Is.EqualTo(cellValue));
+        }
+
+        [Test]
+        public void CellHasInvalidData_ThrowsInvalidOperationException()
+        {
+            //SETUP
+            var cellValueAsString = _fixture.Create<string>();
+            var sut = new CellDataParser();
+
+            //TEST
+            void TestDelegate() => sut.Parse(typeof(double), cellValueAsString);
+
+            //VALIDATE
+            var ex = Assert.Throws<InvalidOperationException>(TestDelegate);
+            Assert.That(ex.Message, Is.EqualTo($"Could not parse {cellValueAsString} to {typeof(double).Name}"));
+        }
+
+        private Fixture _fixture;
+    }
+}
---
+++ src/NinjaCsv.UnitTests/ColumnTests/CtorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using AutoFixture;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace NinjaCsv.UnitTests.ColumnTests
+{
+    [TestFixture]
+    public class CtorTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void ValueIsLessThan0_ThrowsArgumentException()
+        {
+            //SETUP
+            //TEST
+            void TestDelegate() => new Column(_fixture.Create<int>() * -1);
+
+            //VALIDATE
+            Assert.Throws<ArgumentException>(TestDelegate);
+        }
+
+        [Test]
+        public void ValueIs0_ReturnsInstance()
+        {
+            //SETUP
+            //TEST
+            var result = new Column(0);
+
+            //VALIDATE
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void ValueIsGreaterThan0_ReturnsInstance()
+        {
+            //SETUP
+            //TEST
+            var result = new Column(_fixture.Create<int>());
+
+            //VALIDATE
+            Assert.That(result, Is.Not.Null);
+        }
+
+        private Fixture _fixture;
+    }
+}
