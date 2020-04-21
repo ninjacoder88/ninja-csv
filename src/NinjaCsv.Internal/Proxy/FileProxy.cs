305e34c6fc7015310e86ecf1c3201d1516cf8c07
Tom Harris <thomasharris88@gmail.com>
2020-04-20 19:46:33 -0500

    Added proxies

---
+++ src/NinjaCsv.Internal/Proxy/FileProxy.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using NinjaCsv.Internal.Interfaces;
+
+namespace NinjaCsv.Internal.Proxy
+{
+    internal class FileProxy : IFile
+    {
+        public bool Exists(string filePath)
+        {
+            return !string.IsNullOrEmpty(filePath) && File.Exists(filePath);
+        }
+
+        public string[] ReadAllLines(string filePath)
+        {
+            return string.IsNullOrEmpty(filePath) ? new string[0] : File.ReadAllLines(filePath);
+        }
+    }
+}
