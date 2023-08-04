be8877cbd076691e2b6f5b37d9a9a4ae7116459f
Tom Harris <tomharris88@outlook.com>
2023-08-03 22:34:50 -0500

    Added tests

---
+++ src/NinjaCsv.IntegrationTests/CsvCreatorTests/CreateTests.cs
@@ -0,0 +1,38 @@
+﻿using NinjaCsv.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaCsv.IntegrationTests.CsvCreatorTests
+{
+    [TestFixture]
+    internal class CreateTests
+    {
+        [Test]
+        public void Test()
+        {
+            var items = new List<Item>
+            {
+                new Item { Id = 1, Name = "A"},
+                new Item { Id = 2, Name = "B"},
+                new Item { Id = 3, Description = "C"},
+            };
+
+            var creator = new CsvCreator();
+            creator.Create(@"c:\temp\result.csv", items);
+        }
+    }
+
+    public class Item
+    {
+        [Column(0)]
+        public int Id { get; set; }
+
+        [Column(1)]
+        public string Name { get; set; }
+
+        [Column(2)]
+        public string Description { get; set; }
+    }
+}
