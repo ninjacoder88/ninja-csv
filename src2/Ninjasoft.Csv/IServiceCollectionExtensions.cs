46552ce889197b44478bba4dc60007e07d8a3d07
Tom Harris <tomharris88@outlook.com>
2023-11-26 14:26:23 -0600

    Added service registrations

---
+++ src2/Ninjasoft.Csv/IServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ninjasoft.Csv
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCsv(this IServiceCollection services)
+        {
+            return services.AddScoped<ICsvReader, CsvReader>()
+                .AddScoped<ICsvCreator, ICsvCreator>();
+        }
+    }
+}
